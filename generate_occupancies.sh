5fd557b1f88b5bbee64369bf3419c2351deb01fc
Pepe Ojeda <ojedamorala@hotmail.com>
2024-10-08 13:56:50 +0200

    changed occupancies to the correct heights :(

--- generate_occupancies.sh
+++ generate_occupancies.sh
@@ -6,17 +6,10 @@ function run()
     ros2 launch pmfs_env gaden_preproc_launch.py floor_height:=$1
 }
 
-run 0.05
-run 0.15
-run 0.20
-run 0.25
-run 0.30
-run 0.35
-run 0.40
-run 0.45
-run 0.50
-run 0.55
-run 0.65
-run 0.70
-run 0.75
-run 0.80
+height="0.0"
+increment=$(echo 0.08 | bc)
+for i in {1..12}
+do
+    run $(printf "%0.2f" $height)
+    height=$(echo $height+$increment | bc) 
+done
