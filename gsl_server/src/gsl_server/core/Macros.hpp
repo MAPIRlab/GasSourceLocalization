57d139b104622299febda63959f5c6a460e1f01e
PepeOjeda <ojedamorala@hotmail.com>
2026-03-20 09:34:47 +0100

    iwyu pragmas

--- gsl_server/src/gsl_server/core/Macros.hpp
+++ gsl_server/src/gsl_server/core/Macros.hpp
@@ -1,5 +1,5 @@
 #pragma once
-#include "Logging.hpp"
-#include "ConditionalMacros.hpp"
+#include "Logging.hpp" // IWYU pragma: export
+#include "ConditionalMacros.hpp" // IWYU pragma: export
 
 #define CLOSE_PROGRAM raise(SIGTRAP)
